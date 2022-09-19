23a7866a9ac746eb24caccd922aebc42d95e807e
Kamrul hasan <kamrulhasan275723@gmail.com>
2022-09-19 19:41:29 +0600

    second

--- resources/views/create_employee.blade.php
+++ resources/views/create_employee.blade.php
@@ -10,7 +10,7 @@
 </head>
 <body>
     <h2 class="text-center">Create Employee</h2>
-    <form class="container" method="post" action="">
+    <form class="container" method="post" action="{{ url('store-employee') }}">
         @csrf
 
            <div class="form-group">
@@ -37,55 +37,55 @@
             </div>
             
             <div class="form-group">
-               <button id="submitbtn" class="btn btn-primary">Save</button>
+               <button type="submit" id="submitbtn" class="btn btn-primary">Save</button>
             </div>
     </form>
 
-<script>
-    $(document).ready(function(){
-    
-    $("#Division").change(function(){
-        $('#District').empty();
-        var division = $(this).val();
-        
-        $.ajax({
-            url: "http://127.0.0.1:8000/api/get-districts/"+division,
-            type: 'GET',
-            dataType: 'json', 
-            success: function(response) {
-            var districts = response.districts; 
-            var str = '' ;
-            for ( var i = 0 ; i < districts.length ; i++ ){
-                str += '<option value="'+ districts[i].id+'">'+districts[i].name+'</option>';
-            }
+    <script>
+            $(document).ready(function(){
+            
+            $("#Division").change(function(){
+                $('#District').empty();
+                var division = $(this).val();
+                
+                $.ajax({
+                    url: "http://127.0.0.1:8000/api/get-districts/"+division,
+                    type: 'GET',
+                    dataType: 'json', 
+                    success: function(response) {
+                    var districts = response.districts; 
+                    var str = '' ;
+                    for ( var i = 0 ; i < districts.length ; i++ ){
+                        str += '<option value="'+ districts[i].id+'">'+districts[i].name+'</option>';
+                    }
 
-            $('#District').append(str);
+                    $('#District').append(str);
 
+                    }
+                });
+            });
+
+            
+            $('#submitbtn').click(function(){
+
+                $.ajax({
+                    type: "POST",
+                    dataType: 'json' , 
+                    url: "http://127.0.0.1:8000/api/store-employee",
+                    data: {
+                        name : $('#name').val(),
+                        division : $('#Division').val(),
+                        district : $('#District').val()
+                    },
+                
+                    success: function(data){
+                    console.log(data);
             }
+            });
+
         });
-    });
-
-     
-     $('#submitbtn').click(function(){
-
-        $.ajax({
-            type: "POST",
-            dataType: 'json' , 
-            url: "http://127.0.0.1:8000/api/store-employee",
-            data: {
-                name : $('#name').val(),
-                division : $('#Division').val(),
-                district : $('#District').val()
-            },
-         
-            success: function(data){
-            console.log(data);
-       }
-    });
-
-  });
-});
-       
+        });
+            
         
     </script>
     
